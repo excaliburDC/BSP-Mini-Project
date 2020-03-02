6e4aee6806092b99efe645182a2aca1d83993134
DC <43692686+excaliburDC@users.noreply.github.com>
2020-03-02 13:26:34 +0530

    Updated Loading screen

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,7 +7,9 @@ using UnityEngine.UI;
 public class LevelLoader : MonoBehaviour
 {
     public GameObject loadingScreen;
+    public GameObject menuScreen;
     public Slider loadingBar;
+    public Text loadCompleteText;
 
     private void Awake()
     {
@@ -23,16 +25,32 @@ public class LevelLoader : MonoBehaviour
 
    private IEnumerator AsynchronousLoading(int sceneIndex)
    {
+        yield return new WaitForSeconds(5f);
+
         AsyncOperation op = SceneManager.LoadSceneAsync(sceneIndex);
 
+        menuScreen.SetActive(false);
         loadingScreen.SetActive(true);
+        
 
         while(!op.isDone)
         {
-            float progress = Mathf.Clamp01(op.progress / 0.9f);
-            loadingBar.value = progress;
-            Debug.Log(progress);
-            yield return null;
+            loadingBar.value = op.progress;
+            Debug.Log(op.progress);
+
+            // Check if the load has finished
+            if (op.progress >= 0.9f)
+            {
+                loadingBar.enabled = false;
+                //Change the Text to show the Scene is ready
+                loadCompleteText.text = "Press the space bar to continue";
+                //Wait to you press the space key to activate the Scene
+                if (Input.GetKeyDown(KeyCode.Space))
+                    //Activate the Scene
+                    op.allowSceneActivation = true;
+            }
+
+            yield return new WaitForSeconds(5f);
         }
    }
 }
