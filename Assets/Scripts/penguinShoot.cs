3645c835ddab679a7674f14e5997a22ebb9139ac
Stephenjacob2000 <36294217+Stephenjacob2000@users.noreply.github.com>
2020-03-11 11:14:14 +0530

    jythgujygul

---
+++ Assets/Scripts/penguinShoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class penguinShoot : MonoBehaviour
+{
+    public GameObject spawnBalls;
+    public GameObject ballPrefab;
+    public Queue<GameObject> ballList = new Queue<GameObject>();
+   
+    private void createObjects()
+    {
+        for(int i = 0; i<10; i++)
+        {
+            var newObj = Instantiate(ballPrefab,transform.position, Quaternion.identity);
+            newObj.SetActive(false);
+            ballList.Enqueue(newObj);
+        }
+    }
+
+    public void Start()
+    {
+        createObjects();
+    }
+
+    public void addToQueue(GameObject newObj)
+    {
+        newObj.SetActive(false);
+            ballList.Enqueue(newObj);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var spawned = ballList.Dequeue();
+            spawned.transform.position = spawnBalls.transform.position;
+            spawned.SetActive(true);
+        }
+    }
+}
---
+++ Assets/instantiation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiation : MonoBehaviour
+{
+    private Transform lookDir;
+    penguinShoot listUpdater;
+    //Vector3 ballDir;
+    public float ballSpeed = 15.0f;
+
+    public void Start()
+    {
+        
+        lookDir = GameObject.Find("Character").GetComponent<Transform>();
+        listUpdater = GameObject.Find("Character").GetComponent<penguinShoot>();
+        
+    }
+    //public void Awake()
+    //{
+        
+    //}
+
+    public void Update()
+    {
+        //transform.Translate(lookDir.forward * 10f );
+        this.GetComponent<Rigidbody>().AddForce(lookDir.forward * ballSpeed, ForceMode.Impulse);
+   }
+
+    public void LateUpdate()
+    {
+        if (listUpdater.ballList.Count < 7)
+        {
+            listUpdater.addToQueue(gameObject);
+        }
+    }
+}
