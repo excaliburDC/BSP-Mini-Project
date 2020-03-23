10f4b926b3ed631fe630fa9f7f35cc83d34cdc3f
DC <43692686+excaliburDC@users.noreply.github.com>
2020-03-23 09:27:57 +0530

    added audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : SingletonManager<AudioManager>
+{
+    public List<Sounds> sounds;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        foreach (Sounds s in sounds)
+        {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.clip = s.clip;
+            s.audioSource.volume = s.volume;
+            s.audioSource.pitch = s.pitch;
+            s.audioSource.loop = s.loop;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Play(string name)
+    {
+        
+    }
+}
