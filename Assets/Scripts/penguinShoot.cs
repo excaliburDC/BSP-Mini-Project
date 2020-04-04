58fb2537073aab6e8d5e711a6d5e88a4e307ce56
Stephenjacob2000 <36294217+Stephenjacob2000@users.noreply.github.com>
2020-04-04 10:43:20 +0530

    snowballs direction fix

--- Assets/Scripts/penguinShoot.cs
+++ Assets/Scripts/penguinShoot.cs
@@ -10,10 +10,12 @@ public class penguinShoot : MonoBehaviour
     public Queue<GameObject> ballList = new Queue<GameObject>();
     RaycastHit hit, getPoint;
     public Vector3 hitPoint;
-   
+    //public Quaternion rotation;
+    public float maxLength;
+
 
     public int playerattackDamage = 50;
-    
+
 
     public void Update()
     {
@@ -22,21 +24,24 @@ public class penguinShoot : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            shootBalls();
+            //shootBalls();
+           
             var spawned = ballList.Dequeue();
             spawned.transform.position = spawnBalls.transform.position;
+            //setBallsDir();
             spawned.SetActive(true);
-            
-           // Invoke("shootBalls", 1f);
+            spawned.transform.rotation = spawnBalls.transform.rotation;
+
+            // Invoke("shootBalls", 1f);
         }
-       
+
     }
     //Object Pooling
     private void createObjects()
     {
-        for(int i = 0; i<10; i++)
+        for (int i = 0; i < 10; i++)
         {
-            var newObj = Instantiate(ballPrefab,transform.position, Quaternion.identity);
+            var newObj = Instantiate(ballPrefab, transform.position, Quaternion.identity);
             newObj.SetActive(false);
             ballList.Enqueue(newObj);
         }
@@ -50,31 +55,42 @@ public class penguinShoot : MonoBehaviour
     public void addToQueue(GameObject newObj)
     {
         newObj.SetActive(false);
-            ballList.Enqueue(newObj);
+        ballList.Enqueue(newObj);
     }
+
+    //public void setBallsDir()
+    //{
+
+    //    if (Physics.Raycast(spawnBalls.transform.position, spawnBalls.transform.forward, out hit, maxLength))
+    //    {
+    //        var drcn = hit.point - gameObject.transform.position;
+    //        rotation = Quaternion.LookRotation(drcn);
+    //        Debug.DrawRay(spawnBalls.transform.position, spawnBalls.transform.forward, Color.black);
+    //    }
+    //}
     //snowShoot
-    public void shootBalls()
-    {
-        Vector3 camPos = cam.transform.position;
-        camPos.y = camPos.y + 50.0f;
-        if(Physics.Raycast(camPos, cam.transform.forward, out getPoint, 1000.0f))
-        hitPoint =  getPoint.point;
+    //public void shootBalls()
+    //{
+    //    Vector3 camPos = cam.transform.position;
+    //    camPos.y = camPos.y + 50.0f;
+    //    if(Physics.Raycast(camPos, cam.transform.forward, out getPoint, 1000.0f))
+    //    hitPoint =  getPoint.point;
+    //    transform.rotation = Quaternion.LookRotation(spawnBalls.transform.forward);
+    //    if(Physics.Raycast(spawnBalls.transform.position, hitPoint , out hit, 1000.0f ))
+    //    {
+    //        Debug.Log(hit.transform.name);
+    //        Debug.DrawRay(spawnBalls.transform.position, cam.transform.forward,Color.black);
 
-        if(Physics.Raycast(spawnBalls.transform.position, hitPoint , out hit, 1000.0f ))
-        {
-            Debug.Log(hit.transform.name);
-            Debug.DrawRay(spawnBalls.transform.position, cam.transform.forward,Color.black);
+    //        //check if the ball hit the enemy
+    //        if(hit.collider.CompareTag("Enemy"))
+    //        {
+    //            GameController.Instance.GiveDamage(playerattackDamage);
+    //            if (GameController.Instance.currentEnemyHealth <= 0)
+    //                Destroy(hit.collider.gameObject);
+    //        }
+    //    }
 
-            //check if the ball hit the enemy
-            if(hit.collider.CompareTag("Enemy"))
-            {
-                GameController.Instance.GiveDamage(playerattackDamage);
-                if (GameController.Instance.currentEnemyHealth <= 0)
-                    Destroy(hit.collider.gameObject);
-            }
-        }
+    //}
 
-    }
 
-   
 }
--- Assets/instantiation.cs
+++ Assets/instantiation.cs
@@ -4,33 +4,19 @@ using UnityEngine;
 
 public class instantiation : MonoBehaviour
 {
-    private Transform lookDir;
-    penguinShoot listUpdater;
-    //Vector3 ballDir;
-    public float ballSpeed = 15.0f;
-    private Rigidbody rb;
+    private penguinShoot listUpdater;
+    //Updated Code
+    public float Speed = 20;
 
     public void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        lookDir = GameObject.Find("Character").GetComponent<Transform>();
         listUpdater = GameObject.Find("Character").GetComponent<penguinShoot>();
-        
     }
-  
-    public void Update()
+    void Update()
     {
-        //transform.Translate(lookDir.forward * 10f );
-        // this.GetComponent<Rigidbody>().AddForce(lookDir.forward * ballSpeed, ForceMode.Force);
-      // transform.Translate(Vector3.MoveTowards(transform.position, listUpdater.hitPoint, 500f) * ballSpeed * Time.deltaTime, Space.World);
-      //  rb.MovePosition(transform.position + (listUpdater.hitPoint * ballSpeed * Time.deltaTime));
-   }
-    public void FixedUpdate()
-    {
-        // transform.Translate(Vector3.MoveTowards(transform.position, listUpdater.hitPoint, 500f) * ballSpeed * Time.deltaTime, Space.World);
-        rb.AddForce(listUpdater.cam.transform.forward * ballSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
-       // rb.AddForce(Vector3.MoveTowards(listUpdater.spawnBalls.transform.position, listUpdater.hitPoint, 2000f) * ballSpeed* Time.fixedDeltaTime); 
+        transform.position += transform.forward * (Speed * Time.deltaTime);
     }
+
     public void LateUpdate()
     {
         if (listUpdater.ballList.Count < 7)
@@ -38,4 +24,42 @@ public class instantiation : MonoBehaviour
             listUpdater.addToQueue(gameObject);
         }
     }
+
 }
+
+//{
+//    private Transform lookDir;
+//    penguinShoot listUpdater;
+//    //Vector3 ballDir;
+//    public float ballSpeed = 15.0f;
+//    private Rigidbody rb;
+
+//    public void Start()
+//    {
+//        rb = GetComponent<Rigidbody>();
+//        lookDir = GameObject.Find("Character").GetComponent<Transform>();
+//        listUpdater = GameObject.Find("Character").GetComponent<penguinShoot>();
+
+//    }
+
+//    public void Update()
+//    {
+//        //transform.Translate(lookDir.forward * 10f );
+//        // this.GetComponent<Rigidbody>().AddForce(lookDir.forward * ballSpeed, ForceMode.Force);
+//      // transform.Translate(Vector3.MoveTowards(transform.position, listUpdater.hitPoint, 500f) * ballSpeed * Time.deltaTime, Space.World);
+//      //  rb.MovePosition(transform.position + (listUpdater.hitPoint * ballSpeed * Time.deltaTime));
+//   }
+//    public void FixedUpdate()
+//    {
+//        // transform.Translate(Vector3.MoveTowards(transform.position, listUpdater.hitPoint, 500f) * ballSpeed * Time.deltaTime, Space.World);
+//        rb.AddForce(listUpdater.cam.transform.forward * ballSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+//       // rb.AddForce(Vector3.MoveTowards(listUpdater.spawnBalls.transform.position, listUpdater.hitPoint, 2000f) * ballSpeed* Time.fixedDeltaTime); 
+//    }
+//    public void LateUpdate()
+//    {
+//        if (listUpdater.ballList.Count < 7)
+//        {
+//            listUpdater.addToQueue(gameObject);
+//        }
+//    }
+
